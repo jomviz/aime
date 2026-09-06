e5efeb7a63552c4e3c19b2e0b803f2a758db0b15
JOMVIZ <mariaclaudiapetit@gmail.com>
2026-09-05 22:14:29 -0300

    Act20260905

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-patio-ingreso",
-      "name": "Patio ingreso",
+      "id": "0-1---patio-de-ingreso",
+      "name": "1 - Patio de Ingreso",
       "levels": [
         {
           "tileSize": 256,
@@ -20,74 +20,23 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "yaw": 0.000164904299969848,
-        "pitch": 0,
-        "fov": 1.3237681912121593
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.2680098312735222,
-          "pitch": 0.2656459187305735,
-          "rotation": 6.283185307179586,
-          "target": "1-cocina-comedor"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -0.7953231825295486,
-          "pitch": 0.29894970229071305,
-          "title": "Revestimiento Piedra",
-          "text": "ambientcg.com"
-        }
-      ]
-    },
-    {
-      "id": "1-cocina-comedor",
-      "name": "Cocina comedor",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -0.25442620859934983,
-          "pitch": 0.44306917242205657,
-          "rotation": 7.853981633974483,
-          "target": "2-galeria"
-        },
-        {
-          "yaw": -2.0462605555411866,
-          "pitch": 0.3033214396922137,
-          "rotation": 4.71238898038469,
-          "target": "0-patio-ingreso"
+          "yaw": 0.07643810761444314,
+          "pitch": 0.29645357876170664,
+          "rotation": 0,
+          "target": "1-2---cocina-comedor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-galeria",
-      "name": "Galeria",
+      "id": "1-2---cocina-comedor",
+      "name": "2 - Cocina Comedor",
       "levels": [
         {
           "tileSize": 256,
@@ -111,30 +60,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.31601191967836684,
-          "pitch": 0.495413773285474,
+          "yaw": -0.6520188300736152,
+          "pitch": 0.42472137427868795,
           "rotation": 0,
-          "target": "3-patio-trasero"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -2.6532486386533947,
-          "pitch": 0.45837671111567246,
-          "title": "Cocina comedor",
-          "text": "<br>"
+          "target": "5-6---patio"
         },
         {
-          "yaw": -0.8743919731815204,
-          "pitch": 0.1537196519205466,
-          "title": "Parrilla",
-          "text": "<br>"
+          "yaw": -2.5866956768840836,
+          "pitch": 0.4400482737956537,
+          "rotation": 0,
+          "target": "0-1---patio-de-ingreso"
+        },
+        {
+          "yaw": -2.2029669319729557,
+          "pitch": 0.32792635352446453,
+          "rotation": 0,
+          "target": "3-4---dormitorio-principal"
         }
-      ]
+      ],
+      "infoHotspots": []
     },
     {
-      "id": "3-patio-trasero",
-      "name": "Patio trasero",
+      "id": "2-3---bao",
+      "name": "3 - Baño",
       "levels": [
         {
           "tileSize": 256,
@@ -158,20 +106,134 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.1612825798052846,
-          "pitch": 0.4217770878166558,
-          "rotation": 4.71238898038469,
-          "target": "2-galeria"
+          "yaw": 1.8810878438141891,
+          "pitch": 0.7327583886672269,
+          "rotation": 0,
+          "target": "1-2---cocina-comedor"
+        },
+        {
+          "yaw": 2.5854371918294614,
+          "pitch": 0.6457007714089933,
+          "rotation": 0,
+          "target": "4-5---dormitorio-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4---dormitorio-principal",
+      "name": "4 - Dormitorio principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5561873998822513,
+          "pitch": 0.330127220221641,
+          "rotation": 0,
+          "target": "4-5---dormitorio-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5---dormitorio-2",
+      "name": "5 - Dormitorio 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1377736028267194,
+          "pitch": 0.5713175388593559,
+          "rotation": 0,
+          "target": "2-3---bao"
+        },
+        {
+          "yaw": 2.6230116093372304,
+          "pitch": 0.5606821541440556,
+          "rotation": 0,
+          "target": "3-4---dormitorio-principal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6---patio",
+      "name": "6 - Patio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2239978947315624,
+          "pitch": 0.31968317775658583,
+          "rotation": 0,
+          "target": "1-2---cocina-comedor"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "AIME",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
